9e30b8c08574687ad7fde005d90b33aea6ad76bf
akashsiripuram <akashsiripuram>
2025-07-02 14:35:04 +0530

    frontend logic done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [messages, setMessages] = useState(["hi there", "hello"]);
+  const [message, setMessage] = useState();
+  const wsRef = useRef();
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080/");
+    ws.onmessage = (event) => {
+      setMessages((m) => [...m, event.data]);
+    };
+    wsRef.current = ws;
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: "red",
+          },
+        })
+      );
+    };
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="h-screen bg-black flex flex-col justify-between">
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="h-[95vh]">
+          {messages.map((msg) => (
+            <div className="mb-9">
+              <span className="bg-white text-black rounded p-4 m-8">{msg}</span>
+            </div>
+          ))}
+        </div>
+        <div className="w-full bg-white flex">
+          <input
+            type="text"
+            className="flex-1 p-4"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              if (!message) return;
+              wsRef.current.send(
+                JSON.stringify({
+                  type: "chat",
+                  payload: {
+                    message: message,
+                  },
+                })
+              );
+            }}
+            className="cursor-pointer bg-purple-600 text-white p-4 rounded-2xl"
+          >
+            Send
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
